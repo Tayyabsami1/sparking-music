62768ce35cbd3c47acf442af792bc72855a5bcf7
Tayyab Kamran Sami <115224619+Tayyabsami1@users.noreply.github.com>
2024-09-27 23:43:31 +0500

    Basic Initialization and Chai page

---
+++ src/app/chai/page.tsx
@@ -0,0 +1,6 @@
+export default function ChaiPage()
+{
+    return (
+        <h1 className="text-center text-3xl mt-8">Hello from Chai page</h1>
+    )
+}   
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div >
+      <h1 className="text-2xl text-center ">Chai aur NextJS</h1>
+    </div>
+  );
+}
