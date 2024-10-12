62daaa30e42003b198271d6d1483f159b8a854d1
Tayyab Kamran Sami <115224619+Tayyabsami1@users.noreply.github.com>
2024-10-12 12:40:40 +0500

    Navbar issue resolved

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,6 +1,6 @@
 'use client';
 import React, { useState } from "react";
-import { HoveredLink, Menu, MenuItem, ProductItem } from "@/components/ui/navbar-menu";
+import { HoveredLink, Menu, MenuItem } from "@/components/ui/navbar-menu";
 import { cn } from "@/utils/cn";
 import Link from "next/link";
 
@@ -15,24 +15,28 @@ const Navbar = ({ className }: { className?: string }) => {
         </Link>
 
         <MenuItem setActive={setActive} active={active} item="Our Courses">
-        <div className="flex flex-col space-y-4 text-sm">
-        <HoveredLink href="/courses">
-          All Courses
-        </HoveredLink>
-        <HoveredLink href="/courses">
-          Basic Music Theory
-        </HoveredLink>
-        <HoveredLink href="/courses">
-          Advanced Composition
-        </HoveredLink>
-        <HoveredLink href="/courses">
-          Song Writing
-        </HoveredLink>
-        <HoveredLink href="/courses">
-          Music Productiona
-        </HoveredLink>
-        </div>
+          <div className="flex flex-col space-y-4 text-sm">
+            <HoveredLink href="/courses">
+              All Courses
+            </HoveredLink>
+            <HoveredLink href="/courses">
+              Basic Music Theory
+            </HoveredLink>
+            <HoveredLink href="/courses">
+              Advanced Composition
+            </HoveredLink>
+            <HoveredLink href="/courses">
+              Song Writing
+            </HoveredLink>
+            <HoveredLink href="/courses">
+              Music Production
+            </HoveredLink>
+          </div>
         </MenuItem>
+        <Link href={'/contact'}>
+          <MenuItem setActive={setActive} active={active} item="Contact Us">
+          </MenuItem>
+        </Link>
 
       </Menu>
     </div>
